da5db31798b401ec6e87b02778a88be2273e65c3
vita newstetter <vitanewstetter@ucla.edu>
2017-05-23 22:33:20 -0700

    add airdrop popout functionality

--- index.js
+++ index.js
@@ -1,198 +1,201 @@
 //students
 
-var  students =[
+var  studentList =[
     {
    "firstname": "Alex",
     "lastname": "Accarino",
-   "website": "#"
+   "website": "https://www.google.com/search?q=alex+accarino+dma+ucla"
 }, {
     "firstname": "Alex",
     "lastname": "Madrid",
-    "website": "#"
+    "website": "https://www.google.com/search?q=alex+madrid+dma+ucla"
 }, {
     "firstname": "Allek",
     "lastname": "Bien",
-    "website": "#"
+    "website": "https://www.google.com/search?q=allek+bien+dma+ucla"
 }, {
     "firstname": "Anisa",
     "lastname": "Bashiri",
-    "website": "#"
+    "website": "https://www.google.com/search?q=anisa+bashiri+dma+ucla"
 }, {
     "firstname": "Austin",
     "lastname": "Yu",
-    "website": "#"
+    "website": "https://www.google.com/search?q=austin+yu+dma+ucla"
 }, {
     "firstname": "Chance",
     "lastname": "Wen",
-    "website": "#"
+    "website": "https://www.google.com/search?q=chance+wen+dma+ucla"
 }, {
     "firstname": "Elena",
     "lastname": "Birgit Dick",
-    "website": "#"
+    "website": "https://www.google.com/search?q=elena+birgit+dick+dma+ucla"
 }, {
     "firstname": "Erica",
     "lastname": "Young",
-    "website": "#"
+    "website": "https://www.google.com/search?q=erica+young+dma+ucla"
 }, {
     "firstname": "Erika",
     "lastname": "Kehrwald",
-    "website": "#"
+    "website": "https://www.google.com/search?q=erica+kehrwald+dma+ucla"
 }, {
     "firstname": "Evan",
     "lastname": "Berger",
-    "website": "#"
+    "website": "https://www.google.com/search?q=evan+berger+dma+ucla"
 }, {
     "firstname": "Greta",
     "lastname": "Lorenz",
-    "website": "#"
+    "website": "https://www.google.com/search?q=greta+lorenz+dma+ucla"
 }, {
     "firstname": "Hillary",
     "lastname": "Cleary",
-    "website": "#"
+    "website": "https://www.google.com/search?q=hillary+cleary+dma+ucla"
 }, {
     "firstname": "Ho Man",
     "lastname": "Leung",
-    "website": "#"
+    "website": "https://www.google.com/search?q=ho+man+leung+dma+ucla"
 }, {
     "firstname": "Hyesoo",
     "lastname": "Shin",
-    "website": "#"
+    "website": "https://www.google.com/search?q=hyesoo+shin+dma+ucla"
 }, {
     "firstname": "Jake",
     "lastname": "Baylor",
-    "website": "#"
+    "website": "https://www.google.com/search?q=jake+baylor+dma+ucla"
 }, {
     "firstname": "Jason",
     "lastname": "Farwell",
-    "website": "#"
+    "website": "https://www.google.com/search?q=jason+farwell+dma+ucla"
 },{
     "firstname": "Jason",
     "lastname": "Spielman",
-    "website": "#"
+    "website": "https://www.google.com/search?q=jason+spielman+dma+ucla"
 }, {
     "firstname": "Jennifer",
     "lastname": "Fletes-Galindo",
-    "website": "#"
+    "website": "https://www.google.com/search?q=jennifer+fletes-galindo+dma+ucla"
 }, {
     "firstname": "Jessica",
     "lastname": "Rae Vergara",
-    "website": "#"
+    "website": "https://www.google.com/search?q=jessica+rae+vergara+dma+ucla"
 }, {
     "firstname": "Jiamei",
     "lastname": "Deng",
-    "website": "#"
+    "website": "https://www.google.com/search?q=jiamei+deng+dma+ucla"
 }, {
     "firstname": "Jordana",
     "lastname": "Attias",
-    "website": "#"
+    "website": "https://www.google.com/search?q=jordana+attias+dma+ucla"
 }, {
     "firstname": "Justin",
     "lastname": "Piotroski",
-    "website": "#"
+    "website": "https://www.google.com/search?q=justin+piotroski+dma+ucla"
 }, {
     "firstname": "Justine",
     "lastname": "Sto Thomas",
-    "website": "#"
+    "website": "https://www.google.com/search?q=justine+sto+thomas+dma+ucla"
 }, {
     "firstname": "Kain",
     "lastname": "Suwannaphin",
-    "website": "#"
+    "website": "https://www.google.com/search?q=kain+suwannaphin+dma+ucla"
 }, {
     "firstname": "Kayla",
     "lastname": "Eber",
-    "website": "#"
+    "website": "https://www.google.com/search?q=kayla+eber+dma+ucla"
 }, {
     "firstname": "Kelly",
     "lastname": "Brennan",
-    "website": "#"
+    "website": "https://www.google.com/search?q=kelly+brennan+dma+ucla"
 }, {
     "firstname": "Mac",
     "lastname": "Le",
-    "website": "#"
+    "website": "https://www.google.com/search?q=mac+le+dma+ucla"
 }, {
     "firstname": "Maddie",
     "lastname": "Avjean",
-    "website": "#"
+    "website": "https://www.google.com/search?q=maddie+avjean+dma+ucla"
 }, {
     "firstname": "Michael",
     "lastname": "Becker",
-    "website": "#"
+    "website": "https://www.google.com/search?q=michael+becker+dma+ucla"
 }, {
     "firstname": "Nathan",
     "lastname": "Chan",
-    "website": "#"
+    "website": "https://www.google.com/search?q=nathan+chan+dma+ucla"
 }, {
     "firstname": "Nathan",
     "lastname": "Dwarika",
-    "website": "#"
+    "website": "https://www.google.com/search?q=nathan+dwarika+dma+ucla"
 }, {
     "firstname": "Noor",
     "lastname": "Euceph",
-    "website": "#"
+    "website": "https://www.google.com/search?q=noor+euceph+dma+ucla"
 }, {
     "firstname": "Palmo",
     "lastname": "Farber",
-    "website": "#"
+    "website": "https://www.google.com/search?q=palmo+farber+dma+ucla"
 }, {
     "firstname": "Peter",
     "lastname": "Ju",
-    "website": "#"
+    "website": "https://www.google.com/search?q=peter+ju+dma+ucla"
 }, {
     "firstname": "Qiqi",
     "lastname": "Zuo",
-    "website": "#"
+    "website": "https://www.google.com/search?q=qiqi+zuo+dma+ucla"
 }, {
     "firstname": "Rob Tom",
     "lastname": "Browning",
-    "website": "#"
+    "website": "https://www.google.com/search?q=rob+tom+browning+dma+ucla"
 }, {
     "firstname": "Ryan",
     "lastname": "Thomas",
-    "website": "#"
+    "website": "https://www.google.com/search?q=ryan+thomas+dma+ucla"
 }, {
     "firstname": "Sara",
     "lastname": "Haas",
-    "website": "#"
+    "website": "https://www.google.com/search?q=sara+haas+dma+ucla"
 }, {
     "firstname": "Syd",
     "lastname": "Rein",
-    "website": "#"
+    "website": "https://www.google.com/search?q=syd+rein+dma+ucla"
 }, {
     "firstname": "Tara",
     "lastname": "Dilloughery",
-    "website": "#"
+    "website": "https://www.google.com/search?q=tara+dilloughery+dma+ucla"
 }, {
     "firstname": "Tiffany",
     "lastname": "Taimoorazy",
-    "website": "#"
+    "website": "https://www.google.com/search?q=tiffany+taimoorazy+dma+ucla"
 }, {
     "firstname": "Vita",
     "lastname": "Newstetter",
-    "website": "#"
+    "website": "https://www.google.com/search?q=vita+newstetter+dma+ucla"
 }, {
     "firstname": "Zichao",
     "lastname": "Xu",
-    "website": "#"
+    "website": "https://www.google.com/search?q=zichao+xu+dma+ucla"
 }];
 
-//show students in document
-
-var selected = null;
+var selected = {id: null, url: '#'};
 var studentSection = document.getElementById('student-section');
-var __dirname ="/SrProjects";
+var __dirname ="";
+
 var toast = document.getElementById('toast');
+var toastProfile = document.getElementById('toast-profile-image');
+var toastDrop = document.getElementById('toast-drop-image');
+var toastSender = document.getElementById('toast-sender');
 
-for(var i=0; i<students.length; i++){
+var accept = document.getElementById('toast-accept');
+var decline = document.getElementById('toast-decline');
 
-    selected = i;
-    var fullname = students[i].firstname + " " + students[i].lastname;
+//add image for each student
+for(var i=0; i<studentList.length; i++){
+    var fullname = studentList[i].firstname + " " + studentList[i].lastname;
     var urlname = fullname.replace(/\s+/g, '-').toLowerCase();
 
     var target = '_self';
-    if (students[i].website !== '#') target = '_blank';
+    if (studentList[i].website !== '#') target = '_blank';
 
     var student = document.createElement('a');
-    student.href = students[i].website;
     student.classList.add('student-profile');
     student.target = target;
     student.id = i;
@@ -206,12 +209,54 @@ for(var i=0; i<students.length; i++){
 
 var allStudents = document.querySelectorAll(".student-profile");
 
+//handle click on student
 for(var j = 0; j<allStudents.length; j++){
     allStudents[j].onclick = function(){
-        studentSection.style.marginLeft = '-370px';
-        studentSection.style.marginRight = '370px';
-        toast.style.opacity = '1';
-        toast.style.right = '20px';
-        console.log(this.id);
+        //if no student is selected, have airdrop slide out
+        if(selected.id == null){
+            toast.style.opacity = '1';
+            toast.style.right = '20px';
+
+            selected.id = this.id;
+            var fullname = studentList[selected.id].firstname + " " + studentList[selected.id].lastname;
+            var urlname = fullname.replace(/\s+/g, '-').toLowerCase();
+
+            toastDrop.src = __dirname + '/assets/drops/'+ urlname + '-drop.png';
+            toastSender.innerHTML = "from " + studentList[selected.id].firstname;
+            selected.url = studentList[selected.id].website;
+        }else{
+            //if a student is already selected, hide that popout, and slide out again with new student info
+            toast.style.opacity = '0';
+            toast.style.right = '-420px';
+            selected.id = this.id;
+            var fullname = studentList[selected.id].firstname + " " + studentList[selected.id].lastname;
+            var urlname = fullname.replace(/\s+/g, '-').toLowerCase();
+
+            //delay change until airdrop slides back onto screen
+            setTimeout(function(){
+                toast.style.opacity = '1';
+                toast.style.right = '20px';
+
+                toastDrop.src = __dirname + '/assets/drops/'+ urlname + '-drop.png';
+                toastSender.innerHTML = "from " + studentList[selected.id].firstname;
+                selected.url = studentList[selected.id].website;
+            }, 600);
+        }
     }
 }
+
+accept.onclick =  function(){
+    console.log(selected.url);
+    window.open(selected.url, '_blank');
+    toast.style.opacity = '1';
+    toast.style.right = '-420px';
+};
+
+decline.onclick =  function(){
+    selected.id = null;
+    selected.url = '#';
+    toast.style.opacity = '0';
+    toast.style.right = '-420px';
+};
+
+
